33ad19f3973ac47498b42eb6733979bde5e60b8c
Alexander Deider <arth.inbox@gmail.com>
2019-11-30 04:19:37 +0700

    First implementation

---
+++ src/Escaper.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace Arth\Util\String;
+
+class Escaper
+{
+  /** * @var string */
+  protected $escape;
+
+  public function __construct(string $escape = '\\')
+  {
+    $this->escape = $escape;
+  }
+
+  public function split(string $delimiter, string $input, int $limit = null): array
+  {
+    $tokens = $this->tokenize($input, [$delimiter]);
+
+    $res  = [];
+    $flag = false;
+    $word = '';
+    foreach ($tokens as $token) {
+      if (null !== $limit && count($res) === $limit - 1) {
+        $word .= $token;
+        continue;
+      }
+      $this->processToken($token, $delimiter, $flag, $word, $res);
+    }
+    $res[] = $word;
+
+    return $res;
+  }
+  public function join(string $delimiter, array $input): string
+  {
+    return implode($delimiter, array_map(function ($str) use ($delimiter) {
+      return $this->encode($str, [$delimiter]);
+    }, $input));
+  }
+
+  public function encode($word, array $list): string
+  {
+    $word = str_replace($this->escape, "{$this->escape}{$this->escape}", $word);
+    foreach ($list as $sep) {
+      $word = str_replace($sep, "{$this->escape}{$sep}", $word);
+    }
+    return $word;
+  }
+
+  protected function tokenize(string $input, array $list)
+  {
+    $parts[] = preg_quote($this->escape, '#');
+    foreach ($list as $sep) {
+      $parts[] = preg_quote($sep, '#');
+    }
+    $parts = implode('|', $parts);
+
+    preg_match_all("#($parts|.+?)#u", $input, $m);
+
+    return $m[0];
+  }
+  private function processToken(string $token, string $delimiter, bool &$flag, string &$word, array &$res): void
+  {
+    switch ($token) {
+      case $this->escape:
+        if ($flag) {
+          $flag = false;
+          $word .= $token;
+        } else {
+          $flag = true;
+        }
+        break;
+      case $delimiter:
+        if ($flag) {
+          $flag = false;
+          $word .= $token;
+        } else {
+          $res[] = $word;
+          $word  = '';
+        }
+        break;
+      default:
+        if ($flag) {
+          $flag = false;
+          $word .= $this->escape;
+        }
+        $word .= $token;
+    }
+  }
+}
---
+++ tests/JoinSplitTest.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Test\Util\String;
+
+use Arth\Util\String\Escaper;
+use Generator;
+use PHPUnit\Framework\TestCase;
+
+class JoinSplitTest extends TestCase
+{
+  public function testSplitLimit(): void
+  {
+    $svc = new Escaper();
+
+    $input = 'a\.1.b\.2.c\.3.d\.4';
+    $this->assertEquals([$input], $svc->split('.', $input, 1));
+    $this->assertEquals(['a.1', 'b.2', 'c\.3.d\.4'], $svc->split('.', $input, 3));
+  }
+
+  /**
+   * @dataProvider splitData
+   * @param string[] $expected
+   */
+  public function testSplit(string $input, array $expected, int $limit = null): void
+  {
+    $svc = new Escaper('#');
+
+    $this->assertEquals($expected, $svc->split('.', $input, $limit));
+  }
+
+  public function splitData(): ?Generator
+  {
+    yield ['', ['']];
+    yield ['a', ['a']];
+    yield ['#a', ['#a']];
+    yield ['#a#.', ['#a.']];
+    yield ['a.', ['a', '']];
+    yield ['a.', ['a.'], 1];
+    yield ['a#.', ['a#.'], 1];
+    yield ['a#.', ['a.'], 2];
+    yield ['a.', ['a', ''], 2];
+  }
+
+  /**
+   * @dataProvider joinSplitData
+   * @param string[] $words
+   */
+  public function testJoinSplit(array $words, string $delimiter, string $escape, string $joinExpected): void
+  {
+    $svc = new Escaper($escape);
+
+    $encoded = $svc->join($delimiter, $words);
+    $this->assertEquals($joinExpected, $encoded);
+
+    $decoded = $svc->split($delimiter, $encoded);
+    $this->assertEquals($words, $decoded);
+  }
+
+  public function joinSplitData(): ?Generator
+  {
+    yield [
+        [''],
+        '.', '\\',
+        'encoded' => '',
+    ];
+    yield [
+        ['.', '\\', '\.', '\\a.\b'],
+        '.', '\\',
+        'encoded' => '\..\\\\.\\\\\..\\\a\.\\\\b', // '\\\\a' === '\\\a' in PHP
+    ];
+    yield [
+        ['При::вет', 'Мир&#'],
+        '::', '&#',
+        'encoded' => 'При&#::вет::Мир&#&#',
+    ];
+  }
+}
